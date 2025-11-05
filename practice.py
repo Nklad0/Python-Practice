8c26f3a4c9233b5d2e8bd3d61b29e449b61fdf29
Nklad0 <nicholas.kladopoulos86@student.qcc.cuny.edu>
2025-11-05 15:30:48 +0000

    11/5 Practice

--- practice.py
+++ practice.py
@@ -1,7 +1,15 @@
+# Writing a text to a file called "example.txt"
 try:
-    print("Nicholas")
-except:
-    print("Name is unlikely")
-finally:
-    print("Good name!")
+    with open("museums.txt", "w", encoding="utf-8") as f:
+        f.write("AMNH\n")
+        f.write("The Met")
+except: IOError
+print(f"Failed to read file:")
 
+
+try:
+    with open("planes.txt", "w", encoding="utf-8") as f:
+        f.write("Boeing 737\n")
+        f.write("Boeing 747\n")
+except: IOError
+print(f"Please try again")
