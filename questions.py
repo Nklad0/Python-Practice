0d662243dbf22b1586dc4d972c80458065ddd840
Nklad0 <nicholas.kladopoulos86@student.qcc.cuny.edu>
2025-11-11 17:32:17 +0000

    Mini Question 1

--- questions.py
+++ questions.py
@@ -1,18 +1,10 @@
+import math
 import random
 
+num = random.randint(1, 100)
+square_root = math.isqrt(8)
 
-def message(p1, p2):
-    for _ in range(p2):
-        print(p1)
-
-def main():
-    input_text = input("Enter a text: ")
-    text = input_text.title()
-    print(f"text = {text}")
-    n = random.randint(1, 10)
-    print(f"n = {n}")
-    print(f"message(text, n) will print the following:")
-    message(text, n)
-
-if __name__ == "__main__":
-    main()
+print(f"Square root of {num} = {square_root}")
+    
+    
+    
