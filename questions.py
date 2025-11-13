efe48ca4a9a5c69799f3ef97ac00494344e75d2d
Nklad0 <nicholas.kladopoulos86@student.qcc.cuny.edu>
2025-11-13 20:15:08 +0000

    Dictionaries Practice

--- questions.py
+++ questions.py
@@ -1,20 +1,9 @@
-class Car:
-    def __init__(self, make, year):
-        self.make = make
-        self.year = year
+stuInfo = {"name": "Nico Kladopoulos", "GPA": "2.3", "age": 18}
+         
 
-    def display_info(self):
-        print(f"Car: {self.make}, Year: {self.year}")
-
-oldCar = Car("Mitsubishi", 2009)
-newCar = Car("Honda", 2020)
-
-oldCar.display_info()
-newCar.display_info()
-
-print("Year of Car 2:", newCar.year)
-print("Year of Car 1:", oldCar.make)
-
+for k, v in stuInfo.items():
+    print(f"{k.upper()}\t{v}")
+print()
 
     
 
