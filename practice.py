1ac34ab6b8c240584fe3b15d7226ed0bf510059e
Nklad0 <nicholas.kladopoulos86@student.qcc.cuny.edu>
2025-11-18 15:49:47 +0000

    Classes Question 1

--- practice.py
+++ practice.py
@@ -1,4 +1,25 @@
-with open('presidents.txt', 'w') as file:
-    file.write("Ronald Reagan")
-    file.write("Donald Trump")
-    file.write("Joe Biden")
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    
+    def display_info(self):
+        print(f"Width:", self.width) 
+        print(f"Height:", self.height)
+
+r1 = Rectangle(0, 0)
+r2 = Rectangle(11, 12)
+
+r1.display_info()
+r2.display_info()
+
+print("Width of r1:", r1.width)
+print("Width of r2:", r2.height)
+
+
+    
+
+
+
+    
