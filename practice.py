6f4578cfb3b4954812b879500167aaa18a875c69
Nklad0 <nicholas.kladopoulos86@student.qcc.cuny.edu>
2025-11-18 16:17:26 +0000

    Classes Question 2

--- practice.py
+++ practice.py
@@ -1,21 +1,20 @@
 
 class Rectangle:
-    def __init__(self, width, height):
+    def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
     
-    def display_info(self):
-        print(f"Width:", self.width) 
-        print(f"Height:", self.height)
+    def __str__(self):
+        return f"Width: {self.width}, Height: {self.height}"
 
-r1 = Rectangle(0, 0)
+r1 = Rectangle()
 r2 = Rectangle(11, 12)
 
-r1.display_info()
-r2.display_info()
 
-print("Width of r1:", r1.width)
-print("Width of r2:", r2.height)
+
+print(r1)
+print(r2)
+
 
 
     
